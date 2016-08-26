9cc2caa916fe95cfd81c862bfd29180c4cbcefad
Sulaiman.Allen <xalerons@gmail.com>
2016-08-26 09:36:04 -0500

    Updated settings file

--- showdark/showdark/settings.py
+++ showdark/showdark/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tickets.apps',
-    'tickets.utilities',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
+    'tickets.apps.TicketsConfig',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
