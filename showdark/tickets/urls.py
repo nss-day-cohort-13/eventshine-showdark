947182255e315956c2bdd9ac7a70646e16e87e68
Hannah Hall <hannahkhall13@gmail.com>
2016-08-26 11:27:17 -0500

    index url and template

--- showdark/tickets/urls.py
+++ showdark/tickets/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.Index.as_view(), name='index'),
+    #
+    # lines for urls with json data
+    #
 ]
