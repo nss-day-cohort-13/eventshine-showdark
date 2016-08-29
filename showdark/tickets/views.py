d5002952e7652c140244cac09a1b0da99e01c2c2
Hannah Hall <hannahkhall13@gmail.com>
2016-08-29 15:58:26 -0500

    url comment

--- showdark/tickets/views.py
+++ showdark/tickets/views.py
@@ -131,21 +131,12 @@ def get_users_events(request, user_id):
 
 
 def get_all_venues(request):
-    user = get_object_or_404(User, pk=user_id)
-    user_events = get_list_or_404(UserEvent, userId=user.id)
-    print(user_events)
-    data = []
     try:
-        for e in user_events:
-            event = Event.objects.get(pk=e.eventId.id)
-            data.append(event)
-    except TypeError:
-        event = Event.objects.get(pk=user_events.eventId.id)
-        data.append(event)
-
-    outgoing_data = serializers.serialize("json", data)
-
-    return HttpResponse(outgoing_data, content_type="application/json")
+        venues = Venue.objects.all()
+        data = serializers.serialize("json", venues)
+        return HttpResponse(data, content_type="application/json")
+    except:
+        return HttpResponse("No venues registered")
 
 
 def register_for_event(request):
