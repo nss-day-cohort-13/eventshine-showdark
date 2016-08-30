ea9618930fbfec090c257c7717c376b32da210cf
Joseph Kane <josephckane@gmail.com>
2016-08-30 13:33:54 -0500

    reword create event using http post

--- showdark/tickets/static/app/controllers/create_event.js
+++ showdark/tickets/static/app/controllers/create_event.js
@@ -6,4 +6,18 @@ angular.module('app').controller("CreateEventCtrl", function ($scope, $http) {
     // get all venues for form dropdown
     $http.get("localhost:8000/tickets/venues")
         .then((res) => $scope.venues = res.data)
+
+    $scope.create_event = function () {
+    	$http.post("localhost:8000/tickets/profile/create_event", {
+    		eventName: $scope.eventName,
+    		description: $scope.description,
+    		city: $scope.city,
+    		beginTime: $scope.beginTime,
+    		endTime: $scope.endTime,
+    		venue: $scope.venue
+    	})
+    	.then((res) => {
+    		console.log("POSTED!");
+    	})
+    }
 })
