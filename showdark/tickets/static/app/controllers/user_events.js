928f4a4c68d9159a08ab713ed8fada11f6c5471a
Hannah Hall <hannahkhall13@gmail.com>
2016-08-30 14:37:14 -0500

    user events on page

--- showdark/tickets/static/app/controllers/user_events.js
+++ showdark/tickets/static/app/controllers/user_events.js
@@ -1,18 +1,22 @@
-angular.module('app').controller("UserEventsCtrl", function ($scope, $http){
+angular.module('app').controller("UserEventsCtrl", function ($scope, $http, $timeout){
 
     $scope.title = "My Events"
-    let current_user_id = // figure out how to get the current user
+    $http.get(`http://localhost:8000/tickets/get_user/`)
+      .then(function success (res){
+        return $scope.current_user = res.data[0];
+      }) // figure out how to get the current user
 
-    $scope.user_events = []
-    $scope.event_venues = []
-
+    $timeout(function events() {
+      $http.get(`http://localhost:8000/tickets/${$scope.current_user.pk}`)
+          .then((res) => $scope.user_events = res.data)
+    }, 2000)
     // Gets all events with the user id of the current user
-    $http.get(`http://localhost:8000/tickets/${current_user_id}`)
-        .then((res) => $scope.user_events = res.data)
+    // $http.get(`http://localhost:8000/tickets/${$scope.current_user.pk}`)
+    //     .then((res) => $scope.user_events = res.data);
 
     // Gets all venues
     $http.get('http://localhost:8000/tickets/venues')
-        .then((res) => $scope.event_venues = res.data)
+        .then((res) => $scope.event_venues = res.data);
 
     // $scope.delete_event = () => {
     //     $http.delete(`http://localhost:8000/tickets/${current_user_id}`)
