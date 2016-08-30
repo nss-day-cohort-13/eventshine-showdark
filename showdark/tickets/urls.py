49725f4c2682129d90cc82ce838ef4a674c3f82a
Hannah Hall <hannahkhall13@gmail.com>
2016-08-30 13:20:47 -0500

    return current user as json data

--- showdark/tickets/urls.py
+++ showdark/tickets/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^$', views.Login.as_view(), name='login'),
     url(r'^logout/', views.logoutUser, name='logout'),
     # request.POST data to send user info to login
-    url(r'^loginUser/', views.loginUser, name='loginUser'),
+    url(r'^profile/', views.loginUser, name='loginUser'),
     # register page
     url(r'^register/', views.Register.as_view(), name='register'),
     # request.POST to send user data to register
@@ -19,7 +19,7 @@ urlpatterns = [
     #
     # url(r'^event/?P<tickets_event.id>[0-9]+)/$')
     # url that appears if login is succesful
-    url(r'^profile', views.Profile.as_view(), name='profile'),
+    # url(r'^profile', views.Profile.as_view(), name='profile'),
     # get request url for user events
     url(r'^(?P<user_id>[0-9]+)/$', views.get_users_events, name='user_events'),
     # get request for all venues
@@ -28,4 +28,5 @@ urlpatterns = [
     # post request to register for an event
     url(r'^event_registration', views.register_for_event, name='event_registration'),
     url(r'^events', views.get_all_events, name="get_all_events"),
+    url(r'^get_user', views.get_current_user, name="get_current_user")
 ]
