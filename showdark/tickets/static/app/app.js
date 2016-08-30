8feaf709f72a955a5c31346d08ca2bcc24d5abe2
Hannah Hall <hannahkhall13@gmail.com>
2016-08-30 16:18:34 -0500

    httpProvider header

--- showdark/tickets/static/app/app.js
+++ showdark/tickets/static/app/app.js
@@ -7,5 +7,4 @@ angular.module('app', ['ngRoute'])
         $httpProvider.defaults.xsrfCookieName = 'csrftoken';
         $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
         $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
     }]);
